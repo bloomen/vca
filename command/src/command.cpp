bfe49dc76fe7893da3470dad717af9c24ea6145c
Christian Blume <chr.blume@gmail.com>
2021-12-21 10:51:14 -0800

    use rest api in command app

--- command/src/command.cpp
+++ command/src/command.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 #include <boost/algorithm/string.hpp>
-
+#include <restclient-cpp/restclient.h>
+#include <served/served.hpp>
 #include <spdlog/pattern_formatter.h>
 #include <spdlog/sinks/stdout_sinks.h>
 #include <spdlog/spdlog.h>
@@ -9,10 +10,12 @@
 #include <vca/config.h>
 #include <vca/filesystem.h>
 #include <vca/logging.h>
-#include <vca/sqlite_userdb.h>
 #include <vca/string.h>
 #include <vca/time.h>
 #include <vca/utils.h>
+#include <vca/json.h>
+
+using json = nlohmann::json;
 
 int
 main(const int, char**)
@@ -27,9 +30,6 @@ main(const int, char**)
         auto cmdline = spdlog::stdout_logger_mt("cmdline_logger");
         cmdline->set_formatter(std::make_unique<spdlog::pattern_formatter>(
             "%v", spdlog::pattern_time_type::local, ""));
-
-        vca::SqliteUserDb user_db{work_dir / "user_data" / "user.db",
-                                  vca::UserDb::OpenType::ReadOnly};
 
         std::string input;
         for (;;)
@@ -58,18 +58,29 @@ main(const int, char**)
                     cmdline->info("Empty search string.\n");
                     continue;
                 }
-
-                const auto file_contents =
-                    vca::FileContents::fromSearch(values);
-
-                cmdline->info("Searching ...\n");
-                vca::Timer timer;
-                const auto results = user_db.search(file_contents);
-                cmdline->info("Search took: {} us\n", timer.us());
-
-                for (const auto& result : results)
+                std::string value;
+                for (const auto& v : values)
                 {
-                    cmdline->info((result.dir / result.file).u8string() + "\n");
+                    value += vca::wide_to_narrow(v);
+                    value += " ";
+                }
+
+                VCA_INFO << "Searching ...";
+                vca::Timer timer;
+                const auto query = "http://localhost:7777/s?v=" + served::query_escape(value);
+                VCA_INFO << "Query: " << query;
+                const auto response = RestClient::get(query);
+                if (response.code != 200)
+                {
+                    VCA_ERROR << "Received error code: " << response.code;
+                    continue;
+                }
+                VCA_INFO << "Search took: " << timer.us() << " us";
+                json j = json::parse(response.body);
+
+                for (const auto& result : j["results"])
+                {
+                    cmdline->info((fs::u8path(result["d"].get<std::string>()) / fs::u8path(result["f"].get<std::string>())).u8string() + "\n");
                 }
             }
             else
