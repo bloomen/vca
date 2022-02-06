bc8041f4dc35f8a4f081843f8b21295982778cad
Christian Blume <chr.blume@gmail.com>
2022-02-06 16:58:29 +1300

    add new filesystem funcs

--- core/vca/filesystem.h
+++ core/vca/filesystem.h
@@ -3,6 +3,7 @@
 #include <filesystem>
 #include <vector>
 
+#include "platform.h"
 #include "string.h"
 
 namespace fs = std::filesystem;
@@ -21,5 +22,53 @@ is_parent_of(const fs::path& parent, const fs::path& child);
 
 std::string
 read_text(std::istream& f, size_t max_byte_count);
+
+inline fs::path
+native_path(const fs::path& path)
+{
+#ifdef VCA_PLATFORM_WINDOWS
+    constexpr auto prefix = R"(\\?\)";
+    constexpr auto prefix_size = 4;
+    const auto str = path.u8string();
+    if (str.size() >= prefix_size &&
+        std::equal(str.begin(),
+                   str.begin() + prefix_size,
+                   prefix,
+                   prefix + prefix_size))
+    {
+        return path;
+    }
+    else
+    {
+        return prefix + str;
+    }
+#else
+    return path;
+#endif
+}
+
+inline fs::path
+display_path(const fs::path& path)
+{
+#ifdef VCA_PLATFORM_WINDOWS
+    constexpr auto prefix = R"(\\?\)";
+    constexpr auto prefix_size = 4;
+    const auto str = path.u8string();
+    if (str.size() >= prefix_size &&
+        std::equal(str.begin(),
+                   str.begin() + prefix_size,
+                   prefix,
+                   prefix + prefix_size))
+    {
+        return str.substr(prefix_size);
+    }
+    else
+    {
+        return path;
+    }
+#else
+    return path;
+#endif
+}
 
 } // namespace vca
